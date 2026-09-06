f81fde2170ad3a775975a2145ddda5c43731b6cd
Claaaaaay2 <129789716+Claaaaaay2@users.noreply.github.com>
2026-09-06 06:58:09 -0700

    Updated some quest requirements

--- docs/assets/items-CqA0ZeVq.js
+++ docs/assets/items-CqA0ZeVq.js
@@ -1,4 +1,4 @@
-import{f as e}from"./index-_kuWgLSl.js";async function t(){return await e.ensureItemsLoaded(),`
+import{f as e}from"./index-CPKo0dBk.js";async function t(){return await e.ensureItemsLoaded(),`
         <div class="items-header">
             <h1>Items</h1>
             <button id="filter-overrides-toggle" title="Toggle filters and overrides">
